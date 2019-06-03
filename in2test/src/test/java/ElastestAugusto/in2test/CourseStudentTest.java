30de54549c58fcafa4fbd0645aeb4e668b59163f
Cristian Augusto Alonso <cristianaugusto95@gmail.com>
2019-06-03 15:42:35 +0200

    Last Changes

---
+++ in2test/src/test/java/ElastestAugusto/in2test/CourseStudentTest.java
@@ -0,0 +1,104 @@
+package ElastestAugusto.in2test;
+
+import com.fullteaching.e2e.no_elastest.common.BaseLoggedTest;
+import com.fullteaching.e2e.no_elastest.common.CourseNavigationUtilities;
+import com.fullteaching.e2e.no_elastest.common.NavigationUtilities;
+import com.fullteaching.e2e.no_elastest.common.exception.BadUserException;
+import com.fullteaching.e2e.no_elastest.common.exception.ElementNotFoundException;
+import com.fullteaching.e2e.no_elastest.common.exception.NotLoggedException;
+import com.fullteaching.e2e.no_elastest.common.exception.TimeOutExeception;
+import com.fullteaching.e2e.no_elastest.utils.Click;
+import com.fullteaching.e2e.no_elastest.utils.ParameterLoader;
+import com.fullteaching.e2e.no_elastest.utils.Wait;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.fullteaching.e2e.no_elastest.common.Constants.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class CourseStudentTest extends BaseLoggedTest {
+	
+	public String roles;
+	
+
+    public static Stream<Arguments> data() throws IOException {
+        return ParameterLoader.getTestStudents();
+    }
+    
+    
+    @ParameterizedTest
+	@MethodSource("data")
+    public void studentCourseMainTest(String user, String password, String role, RemoteWebDriver rwd)throws ElementNotFoundException, BadUserException, NotLoggedException, TimeOutExeception {
+
+		driver = rwd;
+		driver = loginAndValidate(driver,  user, password);
+
+    	try {
+    		if(!NavigationUtilities.amIHere(driver,COURSES_URL.replace("__HOST__", host)))
+        		driver = NavigationUtilities.toCoursesHome(driver);
+	    	
+    		//go to first course
+    		//get course list
+    		List<String>course_list = CourseNavigationUtilities.getCoursesList(driver, host);
+    		if (course_list.size()<0)  fail("No courses available for test user");
+    		
+    		WebElement course_button = CourseNavigationUtilities.getCourseElement(driver, course_list.get(0)).findElement(By.className("title"));
+    			    	
+	    	driver = Click.element(driver, course_button);
+	    	
+	    	Wait.notTooMuch(driver).until(ExpectedConditions.visibilityOfElementLocated(By.id(TABS_DIV_ID)));
+	    	
+    	}catch(Exception e) {
+			fail("Failed to load Courses Tabs"+ e.getClass()+ ": "+e.getLocalizedMessage());
+    	}
+    	//Check tabs
+    	//Home tab 
+    	try {
+    		driver = CourseNavigationUtilities.go2Tab(driver, HOME_ICON);
+    		
+    	} catch(Exception e) {
+    		fail("Failed to load home tab"+ e.getClass()+ ": "+e.getLocalizedMessage());
+    	}
+    	
+    	try {
+    		driver = CourseNavigationUtilities.go2Tab(driver, SESSION_ICON);
+    	} catch(Exception e) {
+    		fail("Failed to load session tab"+ e.getClass()+ ": "+e.getLocalizedMessage());
+    	}
+    	
+    	try {
+    		driver = CourseNavigationUtilities.go2Tab(driver, FORUM_ICON);
+    	} catch(Exception e) {
+    		fail("Failed to load forum tab"+ e.getClass()+ ": "+e.getLocalizedMessage());
+    	}
+    	
+    	try {
+    		driver = CourseNavigationUtilities.go2Tab(driver, FILES_ICON);
+    	} catch(Exception e) {
+    		fail("Failed to load files tab"+ e.getClass()+ ": "+e.getLocalizedMessage());
+    	}
+    	
+    	try {
+    		driver = CourseNavigationUtilities.go2Tab(driver, ATTENDERS_ICON);	
+    	} catch(Exception e) {
+    		fail("Failed to load attenders tab"+ e.getClass()+ ": "+e.getLocalizedMessage());
+    	}
+    	
+    }
+    
+
+
+}
